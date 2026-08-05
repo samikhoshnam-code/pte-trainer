247fe6bb1035ac378f1cd89a00742291a806704e
samikhoshnam-code <sami.khoshnam@gmail.com>
2026-08-05 21:12:48 +0330

    Update script.js

--- script.js
+++ script.js
@@ -4,78 +4,96 @@ let score = 0;
 
 function startPractice(){
 
-showQuestion();
+    currentQuestion = 0;
+    score = 0;
+
+    showQuestion();
 
 }
+
 
 
 function showQuestion(){
 
-let q = questions[currentQuestion];
-
-document.getElementById("selected").innerHTML =
-q.type;
+    let q = questions[currentQuestion];
 
 
-document.getElementById("result").innerHTML = `
-
-<h2>${q.question}</h2>
-
-${q.options.map(option =>
-
-`
-<button onclick="checkAnswer('${option}')">
-${option}
-</button>
-
-`
-
-).join("")}
-
-`;
-
-}
+    document.getElementById("selected").innerHTML =
+    q.type;
 
 
+    document.getElementById("result").innerHTML = `
 
-function checkAnswer(answer){
+    <h2>${q.question}</h2>
 
-let q = questions[currentQuestion];
+    ${q.options.map(option =>
+
+    `
+    <button class="answer-btn" 
+    onclick="checkAnswer('${option}')">
+    ${option}
+    </button>
+    `
+
+    ).join("")}
 
 
-if(answer === q.answer){
-
-score++;
-
-alert("Correct ✅");
-
-}
-
-else{
-
-alert("Wrong ❌");
+    `;
 
 }
 
 
-currentQuestion++;
+
+function checkAnswer(selectedAnswer){
+
+    let q = questions[currentQuestion];
 
 
-if(currentQuestion < questions.length){
+    console.log("Selected:", selectedAnswer);
+    console.log("Correct:", q.answer);
 
-showQuestion();
 
-}
+    if(selectedAnswer.trim() === q.answer.trim()){
 
-else{
+        score++;
 
-document.getElementById("result").innerHTML=
+        alert("Correct ✅");
 
-`
-<h2>Finished 🎉</h2>
-<p>Your Score: ${score}/${questions.length}</p>
-`;
+    }
 
-}
+    else{
+
+        alert(
+        "Wrong ❌\nCorrect answer: "
+        + q.answer
+        );
+
+    }
+
+
+    currentQuestion++;
+
+
+    if(currentQuestion < questions.length){
+
+        showQuestion();
+
+    }
+
+    else{
+
+
+        document.getElementById("result").innerHTML = `
+
+        <h2>Practice Completed 🎉</h2>
+
+        <h3>
+        Score: ${score}/${questions.length}
+        </h3>
+
+        `;
+
+
+    }
 
 }
