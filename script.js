232d289030513ad50fb105e6e8412f7c2aadf554
samikhoshnam-code <sami.khoshnam@gmail.com>
2026-08-05 21:03:02 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,81 @@
-function openModule(name) {
+let currentQuestion = 0;
+let score = 0;
 
-    document.getElementById("selected").innerHTML =
-        "Selected Module: " + name;
+
+function startPractice(){
+
+showQuestion();
+
+}
+
+
+function showQuestion(){
+
+let q = questions[currentQuestion];
+
+document.getElementById("selected").innerHTML =
+q.type;
+
+
+document.getElementById("result").innerHTML = `
+
+<h2>${q.question}</h2>
+
+${q.options.map(option =>
+
+`
+<button onclick="checkAnswer('${option}')">
+${option}
+</button>
+
+`
+
+).join("")}
+
+`;
+
+}
+
+
+
+function checkAnswer(answer){
+
+let q = questions[currentQuestion];
+
+
+if(answer === q.answer){
+
+score++;
+
+alert("Correct ✅");
+
+}
+
+else{
+
+alert("Wrong ❌");
+
+}
+
+
+currentQuestion++;
+
+
+if(currentQuestion < questions.length){
+
+showQuestion();
+
+}
+
+else{
+
+document.getElementById("result").innerHTML=
+
+`
+<h2>Finished 🎉</h2>
+<p>Your Score: ${score}/${questions.length}</p>
+`;
+
+}
 
 }
