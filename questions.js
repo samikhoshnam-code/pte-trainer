7b2f6dca46b15cda0dfa79bbfcb90df5a57ed105
samikhoshnam-code <sami.khoshnam@gmail.com>
2026-08-05 21:01:21 +0330

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,42 @@
+const questions = [
+
+{
+id:1,
+type:"RW FIB",
+question:"The company has _____ a new strategy to improve performance.",
+options:[
+"adopted",
+"adopting",
+"adopt",
+"adoption"
+],
+answer:"adopted"
+},
+
+{
+id:2,
+type:"RW FIB",
+question:"Technology plays an important _____ in modern education.",
+options:[
+"role",
+"rule",
+"reason",
+"result"
+],
+answer:"role"
+},
+
+{
+id:3,
+type:"RFIB",
+question:"Climate change is one of the most serious _____ facing humanity.",
+options:[
+"issues",
+"issue",
+"issuing",
+"issued"
+],
+answer:"issues"
+}
+
+];
